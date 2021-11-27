667d49d79ee045856db71e5d639737046eab2571
Daniel Guimaraes <daniel@artix-desktop.com>
2021-11-27 12:38:27 -0600

    Added functions to read files

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
-extern crate toml;
 extern crate serde_derive;
+extern crate toml;
+
+use std::{fs, io};
+use std::path::PathBuf;
 
 use serde_derive::Deserialize;
 
 #[allow(dead_code)]
-#[derive(Deserialize)]
-#[derive(Debug)]
+#[derive(Deserialize, Debug)]
 struct Config {
     modkey: String,
     keybind: Vec<Keybind>,
 }
 
 #[allow(dead_code)]
-#[derive(Debug)]
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct Keybind {
     command: String,
     value: Option<String>,
@@ -21,14 +22,35 @@ struct Keybind {
     key: String,
 }
 
-fn main() {
-    let config: Config = read_config();
-    println!("{:?}", config);
-    assert_eq!(config.modkey, "Mod4");
+fn main() -> io::Result<()> {
+    Ok(())
 }
 
-fn read_config() -> Config {
-    toml::from_str(r#"
+#[allow(dead_code)]
+fn parse_config(conf: &str) -> Config {
+    toml::from_str(conf).unwrap()
+}
+
+#[allow(dead_code)]
+fn get_config_files(dir: &str, ) -> io::Result<Vec<PathBuf>> {
+    let entries = fs::read_dir(dir)?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, io::Error>>()?;
+
+    Ok(entries)
+}
+
+#[allow(dead_code)]
+fn read_file(file: &PathBuf) -> io::Result<String> {
+    let content = fs::read_to_string(file)?;
+    Ok(content)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const CONF: &str = r#"
 modkey = "Mod4"
 mousekey = "Mod4"
 tags = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -48,6 +70,37 @@ command = "Execute"
 value = "alacritty"
 modifier = ["modkey", "Shift"]
 key = "Return"
+    "#;
 
-    "#).unwrap()
+    #[test]
+    fn test_parse_config() {
+        let config: Config = parse_config(CONF);
+        assert_eq!(config.modkey, "Mod4");
+        assert_eq!(config.keybind.len(), 2)
+    }
+    
+    #[test]
+    fn test_get_config_files() -> io::Result<()> {
+        let entries = get_config_files("config")?;
+        let mut found  = false;
+        let base = PathBuf::from("config/base.toml");
+        for entry in entries.iter() {
+            if &base == entry {
+                found = true;
+                break
+            }
+        }
+        assert!(found);
+        Ok(())
+    }
+    
+    #[test]
+    fn test_read_file() -> io::Result<()> {
+        let file = PathBuf::from("config/test.toml");
+        let file_content = read_file(&file)?;
+        let config = parse_config(&file_content.to_string());
+        assert_eq!(config.modkey, "Mod4");
+        assert_eq!(config.keybind.len(), 2);
+        Ok(())
+    }
 }
